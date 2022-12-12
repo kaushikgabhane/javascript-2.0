030d7796d0bb86468c7e8640adb6010a5cf18c85
kaushikgabhane <kaushikgabhane705@gmail.com>
2022-12-12 20:33:08 +0530

    added js file & changes in html and css file

---
+++ js/sat.js
@@ -0,0 +1,41 @@
+
+//variable
+
+let x = 5;
+let y = 10;
+
+// console.log(x);
+// console.log(y);
+// let z;
+// console.log( z = x+y);
+
+//Array
+
+// let now = [1,5,3,48,6,7,6];
+// console.log(now[5]);
+
+
+//Object
+
+// let object = {
+//     fname : 'kaushik' ,
+//     lname : 'gabhane' ,
+//     Mo_No : '22564565' ,
+//     email : 'email@gmail.com'
+// };
+
+// console.log(object.email);
+
+// console.log(`${x+y}`);
+
+
+// for(let i=0; i<=10; i++){
+//     console.log(`${i} * ${i} * ${i} = ${i*i*i}`);
+// }
+
+for(let i=0; i<=5; i++){
+
+    for(let j=0; j<=i+1; j++){
+        console.log('*');
+    }
+}
