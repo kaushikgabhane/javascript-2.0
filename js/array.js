19e8b5cdd896314a07a2e6a9febcc4b6b549270e
kaushikgabhane <kaushikgabhane705@gmail.com>
2022-12-18 21:44:10 +0530

    Array in js added

---
+++ js/array.js
@@ -0,0 +1,62 @@
+// Declaration
+
+// let arr1 = [1,2,"kaushik",4,5];
+// console.log(arr1);
+
+// length
+// console.log(arr1.length);
+
+// index
+// console.log(arr1[2]);
+
+// last element
+// console.log(arr1[arr1.length-1]);
+
+// replace element
+// arr1[1] = "I got replaced";
+// console.log(arr1);
+
+
+// push
+// arr1.push("I got pushed");
+// console.log(arr1);
+
+// slice
+// console.log(arr1.slice(1,3));
+// console.log(arr1);
+
+
+// splice
+// let fruit = ['apple', 'banana', 'chiku', 'dryFruit'];
+// fruit.splice(1,1,"orange");
+// console.log(fruit);
+
+// concatenation
+let arr1 = [1,2,3,4];
+let arr2 = [5,6,7,8];
+let arr3 = [9,10,11,21];
+// console.log(arr1.concat(arr2.concat(arr3)));
+// console.log(arr1.concat(arr2,arr3));
+
+// fill
+// let arr4 = [1,2,3,4,5,6,8,7,8];
+// arr4.fill(49,5,7);
+// console.log(arr4);
+
+
+// includes
+// console.log(arr4.includes(5,4));
+
+// indexOf
+// console.log(arr4.indexOf(8));
+
+// isArray
+let num = [11,12,13,14,15,16,17];
+// console.log(Array.isArray(num));
+
+
+// join 
+console.log(num.join(" and "));
+console.log(num.join("  "));
+console.log(num.join(" - "));
+console.log(num.join());
