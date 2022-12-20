55c222dcdaf42100b2519ed79224e1cb55b64f76
kaushikgabhane <kaushikgabhane705@gmail.com>
2022-12-20 19:59:18 +0530

    functions updated

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+// normal function
 function sum(){
     let num1 = 10;
     let num2 = 20;
@@ -5,4 +6,39 @@ function sum(){
     console.log("addition of numbers: " , num1+num2)
 }
 
-sum();
+// sum();
+
+
+// function with argument
+function sum(num1,num2){
+    // let num1 = 10;
+    // let num2 = 20;
+
+    console.log("addition of numbers: " , num1+num2)
+}
+
+// sum(5,8);
+
+
+// function with return type
+function sum(num1,num2){
+    // let num1 = 10;
+    // let num2 = 20;
+    let result = num1 + num2 ;
+    return result;
+}
+
+// let add = sum(50,50);
+// console.log(add);
+
+
+// functions on strings
+function URL(url,domain){
+    let con = "https://";
+    let result = con + url + domain ;
+    return result;
+}
+
+let website = URL('kaushikgabhane',543);
+// console.log(website);
+
