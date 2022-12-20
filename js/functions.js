fb7e5d0e401720dafcd7c0df00f5b55449e17ec5
kaushikgabhane <kaushikgabhane705@gmail.com>
2022-12-20 21:30:40 +0530

    functions updated

--- js/functions.js
+++ js/functions.js
@@ -1,44 +1,75 @@
 // normal function
-function sum(){
-    let num1 = 10;
-    let num2 = 20;
+// function sum(){
+//     let num1 = 10;
+//     let num2 = 20;
 
-    console.log("addition of numbers: " , num1+num2)
-}
+//     console.log("addition of numbers: " , num1+num2)
+// }
 
 // sum();
 
 
 // function with argument
-function sum(num1,num2){
-    // let num1 = 10;
-    // let num2 = 20;
+// function sum(num1,num2){
+//     // let num1 = 10;
+//     // let num2 = 20;
 
-    console.log("addition of numbers: " , num1+num2)
-}
+//     console.log("addition of numbers: " , num1+num2)
+// }
 
 // sum(5,8);
 
 
 // function with return type
-function sum(num1,num2){
-    // let num1 = 10;
-    // let num2 = 20;
-    let result = num1 + num2 ;
-    return result;
-}
+// function sum(num1,num2){
+//     // let num1 = 10;
+//     // let num2 = 20;
+//     let result = num1 + num2 ;
+//     return result;
+// }
 
 // let add = sum(50,50);
 // console.log(add);
 
 
 // functions on strings
-function URL(url,domain){
-    let con = "https://";
-    let result = con + url + domain ;
-    return result;
-}
+// function URL(url,domain){
+//     let con = "https://";
+//     let result = con + url + domain ;
+//     return result;
+// }
 
-let website = URL('kaushikgabhane',543);
+// let website = URL('kaushikgabhane',543);
 // console.log(website);
 
+// passing array in argument
+// function sum(arr){
+//     let sum = 0 ;
+//     for(let i=0;i<arr.length;i++){
+//         sum = sum + arr[i];
+//     }
+//     return sum;
+// }
+// let summary = [1,2,2,4,3,5,6];
+// let arrresult = sum(summary);
+// console.log(arrresult);
+
+// function sum(){
+
+    // this will give array of arguments.
+//     console.log(arguments);    
+//     let sum = 0 ;
+//     for(let i=0;i<arguments.length;i++){
+//         sum = sum + arguments[i];
+//     }
+//     return sum;
+// }
+// this function accepts arguments automatically even if not declared.
+// console.log(sum(21,4,53,65));
+
+// Arrow function
+// let ash = (x,y) => {
+//     console.log("Hii myself",x,y);
+// };
+
+// ash("ash","kechum");
