c05a685b93b9c7ec7754c8d83fa291892fe6d799
kaushikgabhane <kaushikgabhane705@gmail.com>
2022-12-27 23:03:07 +0530

    object file started

---
+++ js/objects.js
@@ -0,0 +1,9 @@
+let customer = {
+    fname : "kaushik",
+    Lname : "gabhane",
+    E_mail: "kau@gmail.com",
+    addess: "Bhandara" 
+};
+
+// console.log(customer);
+console.log(customer.addess);
