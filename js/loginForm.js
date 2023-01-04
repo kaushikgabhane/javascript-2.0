7dae6aa42cedfe56064c861a3324e3bbca05a0f4
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-04 13:10:27 +0530

    login form structure file added

---
+++ js/loginForm.js
@@ -0,0 +1,35 @@
+var fullName = "Kaushik Gabhane";
+
+var uId ="Student at iNeuron";
+
+var email = "kaushik254@gmail.com ";
+
+var mobileNo = "7445525953";
+
+var state = "Maharashtra";
+
+var country = "INDIA";
+
+var password = "Pass@123";
+
+var confirmPassword = " ********** " ;
+
+console.log(`
+    The fullName of student is : ${fullName}
+    
+    Holds the uId which is : ${uId}
+
+    His email is : ${email}
+    
+    Have a mobile number : ${mobileNo}
+    
+    Belong to the state of : ${state}
+    
+    In the country : ${country}
+    
+    Want to Login with password : ${password}
+    
+    and confirmed password : ${confirmPassword}
+    `
+
+);
