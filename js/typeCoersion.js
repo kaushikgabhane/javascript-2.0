10190074dd7e60853f1208470ba48c0400174c06
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-05 15:46:09 +0530

    conditional file typeCoersion

---
+++ js/typeCoersion.js
@@ -0,0 +1,34 @@
+//  Type coersion contains Truthy values and Falsy values
+
+// Truthy values are other than falsy values
+
+// Falsy values are given below :
+
+// undefined  => value is not defined to variable
+
+var result;
+
+console.log(typeof result);
+
+//  null  =>  empty. Does not contain anything.
+
+var array = [];
+
+console.log(`${array}`);
+
+//  0 
+var x = 0 ;
+console.log(x);
+
+
+//  '' => empty string 
+var string = '' ;
+console.log(string.length);
+
+
+
+//  NaN => not a number
+var num1 = "123";
+var num2 = 12345;
+
+console.log(typeof(num1 + num2));
