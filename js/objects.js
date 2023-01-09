24ddb6dfd6c242d75a4831e03e39f8af940ee4cd
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-09 15:08:50 +0530

    nested objects to obj file and index.html added

--- js/objects.js
+++ js/objects.js
@@ -6,4 +6,33 @@ let customer = {
 };
 
 // console.log(customer);
-console.log(customer.addess);
+// console.log(customer.addess);
+
+
+
+
+// nested objects
+
+let student = {
+    name : "ash",
+    class : "5th" ,
+    roll_no : 11 ,
+    marks : {
+        history : 50 ,
+        math : 89,
+        sci : 90,
+
+        extra : {
+            phy_activity : 20,
+            music : 20 ,
+            elocution : 30
+        }
+    }
+};
+
+// console.log(student);
+
+// console.log(student.marks.history);
+
+console.log(student.marks.extra);
+
