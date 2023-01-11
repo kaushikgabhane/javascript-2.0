7953d4c95362bfceda679304bb78475a87f139a3
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-11 21:47:33 +0530

    Que.1 of JS_assignment complete

---
+++ JS_assignment/Que.1/variable.js
@@ -0,0 +1,29 @@
+// string type 
+
+let x = "x";
+
+console.log(`The variable ${x} is of type ${typeof(x)} .`);
+
+
+
+// boolean type
+
+let y = true;
+
+console.log(`The variable ${y} is of type ${typeof(y)} .`);
+
+
+
+// undefined data type
+
+let z;
+console.log(`The type of z is ${z} because we never defined it 😉 .`);
+
+
+
+// null data type
+
+let  zero = null;
+console.log(`The type of zero is ${zero===null}.`);
+
+// As the typeOf null will return 'object' so we have tested it by the above way.
