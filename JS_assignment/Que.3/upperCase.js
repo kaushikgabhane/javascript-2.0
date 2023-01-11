02fa47f5b8ef730d041b35617b763bc1f7433be1
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-11 22:44:38 +0530

    Que.3 of JS_assignment complete

---
+++ JS_assignment/Que.3/upperCase.js
@@ -0,0 +1,5 @@
+// Declare a variable and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+let sweet = "Besan laddu is my favourite sweet .";
+
+console.log(sweet.toUpperCase());
