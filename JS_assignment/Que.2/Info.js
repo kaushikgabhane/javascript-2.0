693cb8e4bd40929ddcf8c4f883ddc39f08e8e0ae
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-11 22:36:11 +0530

    Que.2 of JS_assignment complete

---
+++ JS_assignment/Que.2/Info.js
@@ -0,0 +1,13 @@
+// Declare variables to store your first name, last name, marital status, country and age and display them using interpolation method.
+
+const firstName = "kaushik";
+
+const lastName = "Gabhane";
+
+let maritalStatus = "Unmarried";
+
+const country = "INDIA" ; 
+
+let age = 21;
+
+console.log("The full name of the person is",firstName, lastName ,"with current age ",age , "is" ,maritalStatus ,"and living in the beautiful country known as ",country,"having fun in writing code ." );
