a1f4099b518b23b0f4ff23ef5a430274d549f6a3
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-12 11:36:12 +0530

    Que.6 of JS_assignment complete

---
+++ JS_assignment/Que.6/arrSplit.js
@@ -0,0 +1,7 @@
+// 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' ;
+
+const result = string.split(', ');
+
+console.log(result);
