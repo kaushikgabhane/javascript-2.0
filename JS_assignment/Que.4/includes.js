62023ac53418545e2e5070154a50855c094a7535
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-12 11:07:14 +0530

    Que.4 of JS_assignment complete

---
+++ JS_assignment/Que.4/includes.js
@@ -0,0 +1,5 @@
+// Declare a variable and assign string value to it and check if the string contains a word Script using includes() method.
+
+let  statement = "Java script is one of the best language for development.";
+
+console.log(statement.includes("script"));
