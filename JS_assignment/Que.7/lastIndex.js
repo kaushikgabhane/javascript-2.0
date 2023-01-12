d0d0ad1d2c4b69a52872690f6f1b4f39fe2950ad
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-12 13:10:29 +0530

    Que.7 of JS_assignment complete

---
+++ JS_assignment/Que.7/lastIndex.js
@@ -0,0 +1,5 @@
+// Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+let company = ['pw skills', 'TCS' ,'Wipro','Red hat','iNeuron','pw skills' ,'physics wallah'];
+
+console.log(company.lastIndexOf('pw skills'));
