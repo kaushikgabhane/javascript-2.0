c78fb56ac6af51ed8d7e7474ba9d6552469f2af9
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-03 22:05:30 +0530

    DOM project 5 started

---
+++ DOM assignments/05_DOM Project/05_DOM Project/ans.js
@@ -0,0 +1,56 @@
+
+// This will add a Pro Subscription button.
+const div = document.querySelectorAll(".nav-center div");
+
+const subscription = document.createElement("a");
+subscription.className = "subscription_btn";
+subscription.innerText = "Pro Subscription";
+subscription.style.padding = "12px";
+subscription.style.backgroundColor = "#6C4AB6";
+subscription.style.borderRadius = "5px"
+subscription.style.color = "#fff";
+subscription.style.fontSize = "20px";
+div[div.length-1].appendChild(subscription);
+// console.log(div);
+
+
+// This will add chinese as iteam 7.
+const parent_div = document.querySelector(".tags-container div");
+const chinese = document.createElement("a");
+chinese.setAttribute("href","#");
+chinese.innerText = "Chinese(7)";
+parent_div.appendChild(chinese);
+// console.log(parent_div);
+
+
+
+// This will add  the 6th card.
+const recipe_gallery = document.querySelector(".recipe-gallery");
+
+const card_6 = document.createElement("div");
+card_6.className = "card";
+
+const recipe_text = document.createElement("a");
+recipe_text.className = "recipe-text";
+recipe_text.setAttribute("href","#");
+
+const image = document.createElement("img");
+image.className = "recipe-img";
+image.setAttribute("src","./img/recipe-1.jpeg");
+
+const recipe_name = document.createElement("h5");
+recipe_name.className = "recipe-name";
+recipe_name.innerText = "6th card added";
+
+
+recipe_text.appendChild(image);
+recipe_text.appendChild(recipe_name);
+card_6.appendChild(recipe_text);
+recipe_gallery.appendChild(card_6);
+// console.log(recipe_text);
+
+
+// Add Your Name in footer
+const name1 = document.querySelector(".page-footer p a");
+name1.innerText = "Kaushik Gabhane";
+console.log(name1);
