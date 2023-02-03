22ac0f2a3a6898bc16a891af0f0361e727cf8cac
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-03 23:26:07 +0530

    DOM project 6 started

---
+++ DOM assignments/06_DOM Project/06_DOM Project/script.js
@@ -0,0 +1,17 @@
+const logo = document.querySelector(".logo");
+logo.setAttribute("src","./assets/ineuron-logo.png");
+// console.log(logo);
+
+
+const app_price = document.querySelector(".app_price span");
+app_price.textContent = "$10";
+
+
+const footer_social = document.querySelector(".footer_social");
+
+const footer_social_ico = document.createElement("div");
+footer_social_ico.className = "footer_social_ico";
+footer_social_ico.innerHTML = ` <i class="fa-brands fa-linkedin"></i> `;
+// console.log(footer_social_ico);
+
+footer_social.appendChild(footer_social_ico);
