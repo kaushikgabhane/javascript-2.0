eeb1a3dbe8cc005740f1ec48f233eb66868c0549
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-04 20:25:26 +0530

    DOM project 9 started

---
+++ DOM assignments/DOM P9/DOM P9/script.js
@@ -0,0 +1,14 @@
+// Task 1
+const heading  = document.querySelector(".caption .title");
+heading.style.color = "#D61355"
+
+
+
+
+
+// Task 2
+const cartButton = document.querySelector(".add-to-cart");
+
+cartButton.addEventListener("click", () => {
+    cartButton.style.backgroundColor = "#D61355";
+});
