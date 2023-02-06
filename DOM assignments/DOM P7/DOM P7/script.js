c036edcf76aab5957779ed2228779a0ac19da4fc
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-06 22:26:39 +0530

    proj.7 js file modified

--- DOM assignments/DOM P7/DOM P7/script.js
+++ DOM assignments/DOM P7/DOM P7/script.js
@@ -2,6 +2,14 @@ const all_lang = document.querySelectorAll(".main_languages");
 const all_lang_name = document.querySelectorAll(".main__languages a");
 
 const all_lang_arr = [...all_lang_name];
+console.log(all_lang_arr);
+
+all_lang_arr.forEach((ele) => {
+   return ele.textContent.includes("2.0")? ele.style.display = "none":``;
+});
+
+
+
 
 const input = document.querySelector(".main__form-input");
 // console.log(input);
