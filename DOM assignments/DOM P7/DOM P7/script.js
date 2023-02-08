8deaca2216eb6883adc11d023862cf10cc8ab19b
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-08 12:48:00 +0530

    DOM project 7 complete

--- DOM assignments/DOM P7/DOM P7/script.js
+++ DOM assignments/DOM P7/DOM P7/script.js
@@ -2,7 +2,7 @@ const all_lang = document.querySelectorAll(".main_languages");
 const all_lang_name = document.querySelectorAll(".main__languages a");
 
 const all_lang_arr = [...all_lang_name];
-console.log(all_lang_arr);
+// console.log(all_lang_arr);
 
 all_lang_arr.forEach((ele) => {
    return ele.textContent.includes("2.0")? ele.style.display = "none":``;
@@ -19,7 +19,15 @@ input.setAttribute("placeholder","iNeuron");
 const submit = document.querySelector(".main__form-btn");
 submit.removeAttribute("disabled");
 
-document.addEventListener("load",()=>{
+const form = document.querySelector("form");
 
+form.addEventListener("submit",(e)=>{
+   for(let ele of all_lang_arr){
+      if (ele.textContent.includes("2.0")) {
+         ele.style.display = "inline";
+      }
+   }
+   input.value  = "";
+   e.preventDefault();
 
 });
